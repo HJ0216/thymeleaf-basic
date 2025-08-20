b554e82720201c31e93f1f65b06c339bfd4a1520
HJ0216 <6120hj@gmail.com>
2025-08-20 22:00:45 +0900

    ✨Feat: thymeleaf form 모듈 추가

---
+++ thymeleaf-form/src/main/java/com/hello/thymeleaf/ThymleafFormApplication.java
@@ -0,0 +1,13 @@
+package com.hello.thymleaf_form;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymleafFormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymleafFormApplication.class, args);
+	}
+
+}
---
+++ thymeleaf-form/src/test/java/com/hello/thymeleaf/ThymleafFormApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hello.thymleaf_form;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymleafFormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
