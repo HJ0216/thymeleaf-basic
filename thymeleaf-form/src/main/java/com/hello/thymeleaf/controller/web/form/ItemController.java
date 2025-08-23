0e88671768c6cdd10129d0c8d50d5cf0ac819543
HJ0216 <6120hj@gmail.com>
2025-08-23 12:08:43 +0900

    ✨Feat: 셀렉트 박스 관련 내용 추가

--- thymeleaf-form/src/main/java/com/hello/thymeleaf/controller/web/form/ItemController.java
+++ thymeleaf-form/src/main/java/com/hello/thymeleaf/controller/web/form/ItemController.java
@@ -1,8 +1,10 @@
 package com.hello.thymeleaf.controller.web.form;
 
+import com.hello.thymeleaf.domain.DeliveryCode;
 import com.hello.thymeleaf.domain.Item;
 import com.hello.thymeleaf.domain.ItemType;
 import com.hello.thymeleaf.repository.ItemRepository;
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -38,6 +40,16 @@ public class ItemController {
   @ModelAttribute("itemTypes")
   public ItemType[] itemTypes() {
     return ItemType.values();
+  }
+
+  @ModelAttribute("deliveryCodes")
+  public List<DeliveryCode> deliveryCodes(){
+    List<DeliveryCode> deliveryCodes = new ArrayList<>();
+    deliveryCodes.add(new DeliveryCode("FAST", "빠른 배송"));
+    deliveryCodes.add(new DeliveryCode("NORMAL", "일반 배송"));
+    deliveryCodes.add(new DeliveryCode("SLOW", "느린 배송"));
+
+    return deliveryCodes;
   }
 
   @GetMapping
