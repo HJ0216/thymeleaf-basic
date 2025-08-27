a6eae826274eafe9e9507419b2b0975829fb724f
HJ0216 <6120hj@gmail.com>
2025-08-27 21:44:05 +0900

    ✨Feat: Validation, BindingResult 관련 내용 추가

--- thymeleaf-validation/src/main/java/com/hello/thymeleaf/controller/web/validation/ValidationItemControllerV2.java
+++ thymeleaf-validation/src/main/java/com/hello/thymeleaf/controller/web/validation/ValidationItemControllerV2.java
@@ -2,14 +2,15 @@ package com.hello.thymeleaf.controller.web.validation;
 
 import com.hello.thymeleaf.domain.Item;
 import com.hello.thymeleaf.repository.ItemRepository;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -45,31 +46,34 @@ public class ValidationItemControllerV2 {
     return "validation/v2/addForm";
   }
 
-  @PostMapping("/add")
-  public String addItem(@ModelAttribute Item item, Model model,
-      RedirectAttributes redirectAttributes) {
-    Map<String, String> errors = new HashMap<>();
-
+//  @PostMapping("/add")
+  public String addItemV1(
+      @ModelAttribute Item item, // @ModelAttribute Item item으로 받은 객체는 자동으로 "item"이라는 이름으로 Model에 추가
+      BindingResult bindingResult,
+      Model model,
+      RedirectAttributes redirectAttributes
+  ) {
     if (!StringUtils.hasText(item.getItemName())) {
-      errors.put("itemName", "상품 이름 입력은 필수입니다.");
+      bindingResult.addError(new FieldError("item", "itemName", "상품 이름 입력은 필수입니다."));
     }
     if (item.getPrice() == null || item.getPrice() < 1_000 || item.getPrice() > 1_000_000) {
-      errors.put("price", "가격은 1,000 ~ 1,000,000원까지 허용합니다.");
+      bindingResult.addError(new FieldError("item", "price", "가격은 1,000 ~ 1,000,000원까지 허용합니다."));
     }
     if (item.getQuantity() == null || item.getQuantity() >= 9_999) {
-      errors.put("quantity", "수량은 최대 9,999개까지 허용합니다.");
+      bindingResult.addError(new FieldError("item", "quantity", "수량은 최대 9,999개까지 허용합니다."));
     }
 
     if (item.getPrice() != null && item.getQuantity() != null) {
       int resultPrice = item.getPrice() * item.getQuantity();
       if (resultPrice < 10_000) {
-        errors.put("globalError", "가격*수량의 합은 10,000원 이상이어야 합니다. 현재값: " + resultPrice);
+        // object의 필드가 아닐 경우,
+        bindingResult.addError(
+            new ObjectError("item", "가격*수량의 합은 10,000원 이상이어야 합니다. 현재값: " + resultPrice));
       }
     }
 
-    if (hasError(errors)) {
-      log.info("errors: {}", errors);
-      model.addAttribute("errors", errors);
+    if (bindingResult.hasErrors()) {
+      log.info("errors: {}", bindingResult);
       return "validation/v2/addForm";
     }
 
@@ -79,8 +83,41 @@ public class ValidationItemControllerV2 {
     return "redirect:/validation/v2/items/{itemId}";
   }
 
-  private boolean hasError(Map<String, String> errors) {
-    return !errors.isEmpty();
+  @PostMapping("/add")
+  public String addItemV2(
+      @ModelAttribute Item item, // @ModelAttribute Item item으로 받은 객체는 자동으로 "item"이라는 이름으로 Model에 추가
+      BindingResult bindingResult,
+      Model model,
+      RedirectAttributes redirectAttributes
+  ) {
+    if (!StringUtils.hasText(item.getItemName())) {
+      bindingResult.addError(new FieldError("item", "itemName", item.getItemName(), false, null, null, "상품 이름 입력은 필수입니다."));
+    }
+    if (item.getPrice() == null || item.getPrice() < 1_000 || item.getPrice() > 1_000_000) {
+      bindingResult.addError(new FieldError("item", "price", item.getPrice(), false, null, null, "가격은 1,000 ~ 1,000,000원까지 허용합니다."));
+    }
+    if (item.getQuantity() == null || item.getQuantity() >= 9_999) {
+      bindingResult.addError(new FieldError("item", "quantity", item.getQuantity(), false, null, null, "수량은 최대 9,999개까지 허용합니다."));
+    }
+
+    if (item.getPrice() != null && item.getQuantity() != null) {
+      int resultPrice = item.getPrice() * item.getQuantity();
+      if (resultPrice < 10_000) {
+        // object의 필드가 아닐 경우,
+        bindingResult.addError(
+            new ObjectError("item", null, null, "가격*수량의 합은 10,000원 이상이어야 합니다. 현재값: " + resultPrice));
+      }
+    }
+
+    if (bindingResult.hasErrors()) {
+      log.info("errors: {}", bindingResult);
+      return "validation/v2/addForm";
+    }
+
+    Item savedItem = itemRepository.save(item);
+    redirectAttributes.addAttribute("itemId", savedItem.getId());
+    redirectAttributes.addAttribute("status", true);
+    return "redirect:/validation/v2/items/{itemId}";
   }
 
   @GetMapping("/{itemId}/edit")
