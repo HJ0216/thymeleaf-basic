d536dc9f4c6fe4974240b3383f11a7b7f47fbca2
HJ0216 <6120hj@gmail.com>
2025-08-28 22:01:25 +0900

    ✨Feat: Validated 관련 내용 추가

--- thymeleaf-validation/src/main/java/com/hello/thymeleaf/controller/web/validation/ValidationItemControllerV2.java
+++ thymeleaf-validation/src/main/java/com/hello/thymeleaf/controller/web/validation/ValidationItemControllerV2.java
@@ -11,7 +11,10 @@ import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -26,6 +29,11 @@ public class ValidationItemControllerV2 {
 
   private final ItemRepository itemRepository;
   private final ItemValidator validator;
+
+  @InitBinder
+  public void init(WebDataBinder dataBinder) {
+    dataBinder.addValidators(validator);
+  }
 
   @GetMapping
   public String items(Model model) {
@@ -195,7 +203,7 @@ public class ValidationItemControllerV2 {
     return "redirect:/validation/v2/items/{itemId}";
   }
 
-  @PostMapping("/add")
+//  @PostMapping("/add")
   public String addItemV5(
       @ModelAttribute Item item, // @ModelAttribute Item item으로 받은 객체는 자동으로 "item"이라는 이름으로 Model에 추가
       BindingResult bindingResult,
@@ -206,6 +214,25 @@ public class ValidationItemControllerV2 {
     if (validator.supports(item.getClass())) {
       validator.validate(item, bindingResult);
     }
+
+    if (bindingResult.hasErrors()) {
+      log.info("errors: {}", bindingResult);
+      return "validation/v2/addForm";
+    }
+
+    Item savedItem = itemRepository.save(item);
+    redirectAttributes.addAttribute("itemId", savedItem.getId());
+    redirectAttributes.addAttribute("status", true);
+    return "redirect:/validation/v2/items/{itemId}";
+  }
+
+  @PostMapping("/add")
+  public String addItemV6(
+      @Validated @ModelAttribute Item item, // @ModelAttribute Item item으로 받은 객체는 자동으로 "item"이라는 이름으로 Model에 추가
+      BindingResult bindingResult,
+      Model model,
+      RedirectAttributes redirectAttributes
+  ) {
 
     if (bindingResult.hasErrors()) {
       log.info("errors: {}", bindingResult);
