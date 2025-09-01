5276b630c20801ba7cc883a73b50e7431fcd9e42
HJ0216 <6120hj@gmail.com>
2025-09-01 09:41:06 +0900

    ✨Feat: HttpMessageConverter(@RequestBody) 관련 코드 추가

---
+++ thymeleaf-validation/src/main/java/com/hello/thymeleaf/controller/web/validation/ValidationItemApiController.java
@@ -0,0 +1,30 @@
+package com.hello.thymeleaf.controller.web.validation;
+
+import com.hello.thymeleaf.controller.web.validation.form.ItemSaveForm;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/validation/api/items")
+public class ValidationItemApiController {
+
+  @PostMapping("/add")
+  public Object addItem(@RequestBody @Validated ItemSaveForm form, BindingResult result) {
+
+    log.info("API 컨트롤러 호출");
+
+    if (result.hasErrors()) {
+      log.info("검증 오류 발생: {}", result);
+      return result.getAllErrors();
+    }
+
+    log.info("성공 로직 실행");
+    return form;
+  }
+}
