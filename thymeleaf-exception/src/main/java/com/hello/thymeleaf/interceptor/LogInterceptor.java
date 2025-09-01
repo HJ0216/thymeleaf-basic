58b1037a136bdd948eec7d30ebd8e953a333aab2
HJ0216 <6120hj@gmail.com>
2025-09-01 13:52:28 +0900

    ✨Feat: Exception 처리 및 페이지 관련 코드 추가

---
+++ thymeleaf-exception/src/main/java/com/hello/thymeleaf/interceptor/LogInterceptor.java
@@ -0,0 +1,58 @@
+package com.hello.thymeleaf.interceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.util.UUID;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+@Slf4j
+public class LogInterceptor implements HandlerInterceptor {
+
+  public static final String LOG_ID = "logId";
+
+  @Override
+  public boolean preHandle(
+      HttpServletRequest request,
+      HttpServletResponse response,
+      Object handler
+  ) throws Exception {
+
+    String uuid = UUID.randomUUID().toString();
+    request.setAttribute(LOG_ID, uuid);
+    String requestURI = request.getRequestURI();
+
+    log.info("REQUEST [{}][{}][{}][{}]",
+        uuid, request.getDispatcherType(), requestURI, handler);
+    return true;
+  }
+
+  @Override
+  public void postHandle(
+      HttpServletRequest request,
+      HttpServletResponse response,
+      Object handler,
+      ModelAndView modelAndView
+  ) throws Exception {
+    log.info("postHandle [{}]", modelAndView);
+  }
+
+  @Override
+  public void afterCompletion(
+      HttpServletRequest request,
+      HttpServletResponse response,
+      Object handler,
+      Exception ex
+  ) throws Exception {
+
+    String logId = (String) request.getAttribute(LOG_ID);
+    String requestURI = request.getRequestURI();
+
+    log.info("RESPONSE [{}][{}][{}]",
+        logId, request.getDispatcherType(), requestURI);
+    if (ex != null) {
+      log.error("afterCompletion error!!", ex);
+    }
+  }
+}
